5ea6bc1138f8312fc2759ce394e1365359789a95
umar-uzdenov <90197962+umar-uzdenov@users.noreply.github.com>
2023-03-01 15:37:03 +0600

    Update jadual.js

--- jadual.js
+++ jadual.js
@@ -1,3 +1,24 @@
+(function main() {
+    // const address = "http://127.0.0.1:5500/"
+    // const scripts = [
+    //     "data/data.js",
+    //     "data/time.js",
+    //     "data/day.js",
+    //     "data/lesson.js",
+    //     "data/option.js",
+        
+    //     "utils.js",
+    //     "ui.js",
+    // ]
+
+    // scripts.forEach(name => {
+    //     const tag = document.createElement("script")
+    //     tag.src = address + name
+    //     document.body.appendChild(tag)
+    // })
+
+
+
     const li = document.createElement("li")
     li.className = "nav-item dropdown"
     const a = document.createElement("a")
@@ -5,4 +26,6 @@
     a.textContent = "جدولي - مساعد الحذف والإضافة"
     a.addEventListener("click", () => console.log("launching jadual"))
     li.appendChild(a)
-    document.querySelectorAll("#headerForm>ul")[0].appendChild(li)
+    const ul = document.querySelectorAll("#headerForm>ul")[0]
+    ul.insertBefore(li, ul.firstChild)
+})();
